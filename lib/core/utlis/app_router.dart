cf0488f7538197e445df49933ec7e0d910669b2d
omerbanaemh <111679865+omerbanaemh@users.noreply.github.com>
2025-06-23 10:20:50 +0300

    splashView and routers file

---
+++ lib/core/utlis/app_router.dart
@@ -0,0 +1,33 @@
+abstract class AppRouter {
+  static const kHomeView = '/homeView';
+  static const kBookDetailsView = '/bookDetailsView';
+  static const kSearchView = '/searchView';
+
+  // static final router = GoRouter(
+  //   routes: [
+  //     GoRoute(
+  //       path: '/',
+  //       builder: (context, state) => const SplashView(),
+  //     ),
+  //     GoRoute(
+  //       path: kSearchView,
+  //       builder: (context, state) => const SearchView(),
+  //     ),
+  //     GoRoute(
+  //       path: kHomeView,
+  //       builder: (context, state) => const HomeView(),
+  //     ),
+  //     GoRoute(
+  //       path: kBookDetailsView,
+  //       builder: (context, state) => BlocProvider(
+  //         create: (context) => SimilarBooksCubit(
+  //           getIt.get<HomeRepoImpl>(),
+  //         ),
+  //         child: BookDetailsView(
+  //           bookModel: state.extra as BookModel,
+  //         ),
+  //       ),
+  //     ),
+  //   ],
+  // );
+}
--- lib/presentation/views/widgets/splash_view_body.dart
+++ lib/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,39 @@
+import 'package:bookly/core/utlis/app_router.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utlis/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+
+
+class _SplashViewBodyState extends State<SplashViewBody>  with SingleTickerProviderStateMixin {
+
+
+
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+
+    navigateToHome();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +42,48 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
+        AnimatedBuilder(
+        animation: slidingAnimation,
+        builder: (context, _) {
+          return SlideTransition(
+            position: slidingAnimation,
+            child: const Text(
+              'Read Free Books',
+              textAlign: TextAlign.center,
+            ),
+          );
+        })
       ],
+    );
+  }
+
+
+
+
+    void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+
+    animationController.forward();
+  }
+
+  void navigateToHome() {
+    Future.delayed(
+      const Duration(seconds: 2),
+      () {
+        // Get.to(() => const HomeView(),
+        //     // calculations
+        //     transition: Transition.fade,
+        //     duration: kTranstionDuration);
+
+        GoRouter.of(context).push(AppRouter.kHomeView);
+      },
     );
   }
 }
